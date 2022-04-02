85671f2540c15680663df681677871667a3ff2da
Monika Rafalska <82111615+MonikaRafalska@users.noreply.github.com>
2022-04-03 01:19:36 +0200

    styles

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import Contacts from "../Contacts/Contacts";
 const ContactList = () => {
   return (
-      <ul>
       <Contacts/>
-      </ul>
   );
 };
 export default ContactList;
